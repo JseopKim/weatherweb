68546cedfe41dfe61fbb20c6e8ed4d06820522df
JseopKim <jsub5646@gmail.com>
2023-10-25 00:31:24 +0900

    현재 날씨 div 위치 조정

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import React from 'react';
+import CurrentWeather from './Current';
 
 function App() {
   return (
     <div className="App">
       <div className="App-back">
-        
+        <CurrentWeather />
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
---
+++ weatherapp/src/Current.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Current.css';
+
+function CurrentWeather() {
+  return (
+    <div className='Current'>
+      <h1>Hello from MyComponent</h1>
+    </div>
+  );
+}
+
+export default CurrentWeather;
