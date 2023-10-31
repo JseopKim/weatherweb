4c2e0534064600205ee26a1ca3cd4e8660225110
JseopKim <jsub5646@gmail.com>
2023-11-01 00:32:41 +0900

    날짜를 변수화 하여 현재시각 기준으로 날짜 데이터 가져오기 성공

--- weatherapp/src/WeatherInfo.js
+++ weatherapp/src/WeatherInfo.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3001; // 원하는 포트로 설정
-const currentTime = new Date();
+const current = new Date();
 const options = {hour: '2-digit', minute: '2-digit', second: '2-digit'};
 
-const formattedTime = `${currentTime.toLocaleTimeString('kr-KR', options)}`;
+let year = current.getFullYear();
+//! getMonth()는 0-11을 출력, 2자리로 설정
+let month = (current.getMonth() + 1).toString().padStart(2, '0');
+let date = current.getDate().toString().padStart(2, '0');
+let fullDate = `${year}${month}${date}`
+let currentHour = current.getHours();
+let currentMinute = current.getMinutes();
 
-console.log(formattedTime);
+console.log(typeof(fullDate));
 
 // 미들웨어 설정 (CORS 처리 등)
 app.use(express.json());
@@ -15,7 +21,7 @@ let temeperatureObject;
 
 // API 엔드포인트ß
 app.get('/api/weather', (req, res) => {
-  fetch('https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=YZoS%2BsXeAepKP3LboIXQqhWlKoX9QnyqON1fEs1rtqgf1P5HWoO6IIAxAEL5gI6OSmXgxfMxA%2F5dumJSW2IeHg%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20231031&base_time=0000&nx=55&ny=127')
+  fetch(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=YZoS%2BsXeAepKP3LboIXQqhWlKoX9QnyqON1fEs1rtqgf1P5HWoO6IIAxAEL5gI6OSmXgxfMxA%2F5dumJSW2IeHg%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${fullDate}&base_time=0000&nx=55&ny=127`)
     .then((response) => response.json())
     .then((data) => {
       // API 응답 데이터를 클라이언트에게 반환합니다.
@@ -24,6 +30,7 @@ app.get('/api/weather', (req, res) => {
       res.json(data);
 
       console.log(temeperatureObject);
+      // console.log(fullDate);
     })
     .catch((error) => {
       console.error(error);
