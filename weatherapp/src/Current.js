0f9ff7873ffc4e43dc97876622a7f58f6c8791d6
JseopKim <jsub5646@gmail.com>
2023-11-10 13:29:25 +0900

    Current.js 업데이트

--- weatherapp/src/Current.js
+++ weatherapp/src/Current.js
@@ -1,9 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './Current.css';
 
 function CurrentWeather() {
-
   const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    // API 요청을 보내어 서버로부터 데이터 가져오기
+    fetch('/api/weather') // API 엔드포인트로 설정
+      .then((response) => response.json())
+      .then((data) => {
+        // API로부터 받은 데이터를 상태로 설정
+        // setWeatherData(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
 
   return (
     <div className='Current'>
--- weatherapp/src/WeatherInfo.js
+++ weatherapp/src/WeatherInfo.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3001; // 원하는 포트로 설정
+const port = 3000; // 원하는 포트로 설정
 const current = new Date();
-const options = {hour: '2-digit', minute: '2-digit', second: '2-digit'};
 
 let year = current.getFullYear();
 //! getMonth()는 0-11을 출력, 2자리로 설정
@@ -13,6 +12,7 @@ let fullDate = `${year}${month}${date}`;
 let currentHour = current.getHours().toString().padStart(2, '0');
 let currentMinute = current.getMinutes().toString().padStart(2, '0');
 let fullTime = `${currentHour}${currentMinute}`
+console.log(fullDate);
 console.log(fullTime);
 
 // 미들웨어 설정 (CORS 처리 등)
@@ -20,7 +20,7 @@ app.use(express.json());
 let weatherData;
 let temeperatureObject;
 
-// API 엔드포인트ß
+// API 엔드포인트
 app.get('/api/weather', (req, res) => {
   fetch(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=YZoS%2BsXeAepKP3LboIXQqhWlKoX9QnyqON1fEs1rtqgf1P5HWoO6IIAxAEL5gI6OSmXgxfMxA%2F5dumJSW2IeHg%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${fullDate}&base_time=${fullTime}&nx=55&ny=127`)
     .then((response) => response.json())
